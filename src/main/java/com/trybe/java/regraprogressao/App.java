e19bed951e9945eb1c546a0d8b53a0575eeb8cbf
Patricia Eliseu <patyeliseu4@gmail.com>
2023-07-05 16:57:20 -0300

    iniciando projeto em Java

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -5,9 +5,9 @@ package com.trybe.java.regraprogressao;
  */
 public class App {
 
-  /**
-   * Metodo main.
-   */
-  public static void main(String[] args) {
-  }
+    /**
+     * Metodo main.
+     */
+    public static void main(String[] args) {
+    }
 }
