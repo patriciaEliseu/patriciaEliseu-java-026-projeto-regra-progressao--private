231ea51ec606fb3f7025f8b0e91f1ed38823de71
Patricia Eliseu <patyeliseu4@gmail.com>
2023-07-11 23:29:54 -0300

    a inserção dos inputs

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,13 +1,27 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
  * App.
  */
 public class App {
 
-    /**
-     * Metodo main.
-     */
-    public static void main(String[] args) {
+  /**
+   * Metodo main.
+   */
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Digite a quantidade de atividades para cadastrar: ");
+    String input = scanner.nextLine();
+    int nAtividades = Integer.parseInt(input);
+    for (int i = 1; i <= nAtividades; i++) {
+      System.out.println("Digite o nome da atividade " + i + ":");
+      String nome = scanner.nextLine();
+      System.out.println("Digite o peso da atividade " + i + ":");
+      String peso = scanner.nextLine();
+      System.out.println("Digite a nota obtida para " + i + ":");
+      String nota = scanner.nextLine();
     }
+  }
 }
