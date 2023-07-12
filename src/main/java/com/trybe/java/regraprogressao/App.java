f42ccf551cca99c224dddb61c9e03c2ad3ad7d4a
Patricia Eliseu <patyeliseu4@gmail.com>
2023-07-12 00:36:36 -0300

    conserto lint

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -12,16 +12,17 @@ public class App {
    */
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+
     System.out.print("Digite a quantidade de atividades para cadastrar: ");
     String input = scanner.nextLine();
-    int nAtividades = Integer.parseInt(input);
-    for (int i = 1; i <= nAtividades; i++) {
+    int numeroAtividades = Integer.parseInt(input);
+    for (int i = 1; i <= numeroAtividades; i++) {
       System.out.println("Digite o nome da atividade " + i + ":");
       String nome = scanner.nextLine();
       System.out.println("Digite o peso da atividade " + i + ":");
-      String peso = scanner.nextLine();
+      int peso = Integer.parseInt(scanner.nextLine());
       System.out.println("Digite a nota obtida para " + i + ":");
-      String nota = scanner.nextLine();
+      int nota = Integer.parseInt(scanner.nextLine());
     }
   }
 }
